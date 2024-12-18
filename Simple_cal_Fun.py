b9196aab208101e246f51e9a54646586f9417ced
Manjit <manjitkaur.301091@gmail.com>
2024-12-18 15:48:06 +0530

    simple calculation funtion

---
+++ Simple_cal_Fun.py
@@ -0,0 +1,10 @@
+# simple Program with function to perform calucation 
+from ast import FunctionType
+
+
+Calculation_of_units= 24
+Name_of_Unit="Hous"
+def days_of_units():
+    print (f" 20 days are {20*Calculation_of_units} {Name_of_Unit} ")
+
+days_of_units()    
